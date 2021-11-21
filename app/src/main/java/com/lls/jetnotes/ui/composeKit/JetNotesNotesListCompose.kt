29ec8755f16a4f6b37f7f9c36c9e65d15de467f6
Lucas <limallucas96@gmail.com>
2021-11-21 19:28:32 -0300

    renamed packed

---
+++ app/src/main/java/com/lls/jetnotes/ui/composeKit/JetNotesNotesListCompose.kt
@@ -0,0 +1,48 @@
+package com.lls.jetnotes.ui.composeKit
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.lls.jetnotes.ui.theme.JetNotesTheme
+import entities.Notes
+
+@ExperimentalMaterialApi
+@Preview(showBackground = true)
+@Composable
+fun NotesListComposePreview() {
+    JetNotesTheme {
+        NotesListCompose(listOf(), {}, {})
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun NotesListCompose(
+    notes: List<Notes>,
+    onNoteClicked: (Notes) -> Unit,
+    onDeleteClicked: (Notes) -> Unit
+) {
+    if (notes.isEmpty()) {
+        EmptyStateCompose()
+    } else {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            items(notes, key = { it }) { note ->
+                NoteCardCompose(
+                    note = note.noteText,
+                    onNoteClicked = { onNoteClicked.invoke(note) },
+                    onDeleteClicked = { onDeleteClicked.invoke(note) }
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/lls/jetnotes/ui/screen/listNotes/ListNotesScreen.kt
@@ -0,0 +1,50 @@
+package com.lls.jetnotes.ui.screen.listNotes
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lls.jetnotes.ui.composeKit.AppToolbarCompose
+import com.lls.jetnotes.ui.composeKit.MyApp
+import com.lls.jetnotes.ui.composeKit.NotesListCompose
+import com.lls.jetnotes.ui.theme.JetNotesTheme
+import kotlinx.coroutines.InternalCoroutinesApi
+import org.koin.androidx.compose.getViewModel
+
+@InternalCoroutinesApi
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreviewListNotesScreen() {
+    JetNotesTheme {
+        ListNotesScreen {}
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@InternalCoroutinesApi
+@Composable
+fun ListNotesScreen(onCreateNewNote: (id: Int?) -> Unit = {}) {
+    val viewModel = getViewModel<ListNotesScreenViewModel>()
+
+    val viewState by viewModel.notesFlow.collectAsState()
+
+    MyApp {
+        Box(
+            Modifier.fillMaxWidth()
+        ) {
+            Column {
+                AppToolbarCompose(primaryAction = {
+                    onCreateNewNote.invoke(null)
+                })
+                NotesListCompose(notes = viewState,
+                    onNoteClicked = { onCreateNewNote.invoke(it.id) },
+                    onDeleteClicked = { viewModel.deleteNote(it) })
+            }
+        }
+    }
+}
